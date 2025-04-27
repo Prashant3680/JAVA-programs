558a1399a2867b20cb9b4a08014fb2534508bd8a
Prashant3680 <patilprashant3680@gmail.com>
2025-04-27 17:15:45 +0530

    Add files via upload

---
+++ Operators/Logical operators/Logical1.java
@@ -0,0 +1,13 @@
+class Logical1
+{   public static void main(String[] args)
+    {
+	System.out.println(50<60&&100>90);
+	System.out.println('A'>'a'&&30<40);
+	System.out.println('z'>'G'&&100/2==50);
+	System.out.println(true!=!true);
+
+    }
+
+
+
+}
