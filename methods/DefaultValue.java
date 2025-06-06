b9fd2bcc1a31b81010424dc83db551496a9cfe07
Prashant3680 <patilprashant3680@gmail.com>
2025-06-06 20:52:14 +0530

    Add files via upload

---
+++ methods/DefaultValue.java
@@ -0,0 +1,24 @@
+class DefaultValue
+{
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static String st;
+ public static void main(String[] a)
+ {
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(i);
+	System.out.println(l);
+	System.out.println(f);
+	System.out.println(d);
+	System.out.println(s);
+	System.out.println(c);
+ }
+
+
+}
