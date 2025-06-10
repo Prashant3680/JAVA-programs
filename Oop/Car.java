90fcb29353a7e39ad2beaeeb5a7a601571564b48
Prashant3680 <patilprashant3680@gmail.com>
2025-06-10 20:04:39 +0530

    Add files via upload

---
+++ Oop/Car.java
@@ -0,0 +1,34 @@
+class Car
+{
+ String brand;
+ String name;
+ Double price;
+ String fuel;
+ String type;
+ String colour;
+ Double horsePower;
+ Double maxSpeed;
+ Double acceleration;
+
+ public void getDetails()
+ {
+	System.out.println("Brand:"+brand);
+	System.out.println("Name: "+name);
+	System.out.println("Price: "+price);
+	System.out.println("Fuel: "+fuel);
+	System.out.println("Type: "+type);
+	System.out.println("Colour: "+colour);
+	System.out.println("Horsepower: "+horsePower);
+	System.out.println("MAX Speed: "+maxSpeed+" Km/Hr");
+	System.out.println("Acceleration: "+acceleration);
+ }
+
+ Car()
+ {
+
+ }
+
+
+
+}
+
---
+++ Oop/CarDriver.java
@@ -0,0 +1,18 @@
+class CarDriver
+{
+ public static void main(String[] a)
+ {
+	Car c1=new Car();
+	c1.brand="Lamborghini";
+	c1.name="Aventadoor";
+	c1.price=40000000;
+	c1.fuel="Diesel";
+	c1.type="Convertible";
+	c1.colour="Red";
+	c1.horsePower=500;
+	c1.maxSpeed=380;
+	c1.acceleration=30;
+	c1.getDetails();
+ }
+
+}
