4d0db471545b37345c041a73ac6b58bf857956ce
Prashant3680 <patilprashant3680@gmail.com>
2025-07-01 19:54:34 +0530

    Add files via upload

---
+++ methods/StatBlock1.java
@@ -0,0 +1,20 @@
+class StatBlock1
+{
+ static
+ {
+	System.out.println("from static block 1");
+	System.out.println(a);
+ }
+
+ static int a;
+
+ public static void main(String[] a)
+ {
+	System.out.println("From main");
+ }
+
+
+
+
+
+}
