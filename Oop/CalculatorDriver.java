314fd7baccec53feee88fcb322041902dbb3c1e3
Prashant3680 <patilprashant3680@gmail.com>
2025-07-09 20:21:14 +0530

    Add files via upload

---
+++ Oop/CalculatorDriver.java
@@ -0,0 +1,12 @@
+public class CalculatorDriver 
+{
+ public static void main(String[] args)
+ {
+	    Calculator c1=new CalculatorImp();
+		c1.addition(78,45);
+		c1.subtraction(65,12);
+		c1.division(10, 4);
+		c1.multiplication(4,5);
+}
+	
+}
