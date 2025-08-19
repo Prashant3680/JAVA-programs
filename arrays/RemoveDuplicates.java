ef1c30328fdb644cc634e876e24719d0313ae95f
Prashant3680 <patilprashant3680@gmail.com>
2025-08-19 22:19:31 +0530

    Add files via upload

---
+++ arrays/RemoveDuplicates.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+public class RemoveDuplicates 
+{
+	public static void main(String[] args) 
+	{
+		int[] ar={2,45,76,2,45,7,8,44,61,76,90,79};
+		
+		System.out.println(Arrays.toString(removeDuplicates(ar)));
+	}
+	
+	public static int[] removeDuplicates(int[] ref)
+	{
+		int[] ct=markDuplicates(ref);
+		
+		int[] ans=new int[ref.length-ct[ref.length]];
+		
+		for(int i=0,j=0;i<ref.length;i++)
+		{
+			if(ct[i]!= -1)
+			{
+				ans[j]=ref[i];
+				j++;
+			}
+		}
+		
+		return ans;
+		
+	}
+	
+	public static int[] markDuplicates(int[] ref)
+	{
+		int[] ct=new int[ref.length+1];
+		int count=0;
+		
+		for(int i=0;i<ref.length;i++)
+		{
+			if(ct[i]!= -1)
+			{
+				for(int j=i+1;j<ref.length;j++)
+				{
+					if(ref[i]==ref[j])
+					{
+						ct[j]=-1;
+						count++;
+					}
+				}
+			}
+			
+		}
+		
+		ct[ref.length]=count;
+		return ct;
+	}
+}
